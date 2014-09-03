629ab3e5f67ad861240484eb6bd0e7c432809783
Ryan LeViseur <vidkun@vlfservices.com>
2014-09-03 15:13:07 -0600

    Add ordering functionality - generate order model and controller - associate order and line items - set up form for capturing order details - validate form details - create new order records in db and clear cart - add ajax to remove note once item added to cart after   a successfull order is placed.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902153645) do
+ActiveRecord::Schema.define(version: 20140903200006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,10 +28,21 @@ ActiveRecord::Schema.define(version: 20140902153645) do
     t.datetime "updated_at"
     t.integer  "quantity",                           default: 1
     t.decimal  "price",      precision: 8, scale: 2
+    t.integer  "order_id"
   end
 
   add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id", using: :btree
+  add_index "line_items", ["order_id"], name: "index_line_items_on_order_id", using: :btree
   add_index "line_items", ["product_id"], name: "index_line_items_on_product_id", using: :btree
+
+  create_table "orders", force: true do |t|
+    t.string   "name"
+    t.text     "address"
+    t.string   "email"
+    t.string   "pay_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
